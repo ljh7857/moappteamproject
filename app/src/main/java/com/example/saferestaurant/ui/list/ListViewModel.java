406bb95fcd0b020f0cad07e7bd7c0849db8d375f
gon125 <geonhyeong.lim@icloud.com>
2020-11-04 17:09:16 +0900

    Update ui

--- app/src/main/java/com/example/saferestaurant/ui/list/ListViewModel.java
+++ app/src/main/java/com/example/saferestaurant/ui/list/ListViewModel.java
@@ -1,16 +1,16 @@
-package com.example.saferestaurant.ui.notifications;
+package com.example.saferestaurant.ui.list;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class ListViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public ListViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is dashboard fragment");
     }
 
     public LiveData<String> getText() {
--- app/src/main/java/com/example/saferestaurant/ui/map/MapFragment.java
+++ app/src/main/java/com/example/saferestaurant/ui/map/MapFragment.java
@@ -1,4 +1,4 @@
-package com.example.saferestaurant.ui.home;
+package com.example.saferestaurant.ui.map;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.saferestaurant.R;
 
-public class HomeFragment extends Fragment {
+public class MapFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private MapViewModel mapViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        mapViewModel =
+                ViewModelProviders.of(this).get(MapViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+        final TextView textView = root.findViewById(R.id.text_map);
+        mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);

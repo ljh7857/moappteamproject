900d71604d9a11c0299473910eefabef905ecc49
gon125 <geonhyeong.lim@icloud.com>
2020-11-05 20:27:52 +0900

    Delete listFragment refactoring

--- app/src/main/java/com/moappohjo/saferestaurant/ui/MainViewModel.java
+++ app/src/main/java/com/moappohjo/saferestaurant/ui/MainViewModel.java
@@ -1,14 +1,8 @@
 package com.moappohjo.saferestaurant.ui;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-
 import com.moappohjo.saferestaurant.ui.helper.CardViewItem;
 import com.moappohjo.saferestaurant.ui.helper.ListLiveData;
 import com.naver.maps.map.overlay.Marker;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainViewModel extends androidx.lifecycle.ViewModel {
     ListLiveData<CardViewItem> items = new ListLiveData<>();

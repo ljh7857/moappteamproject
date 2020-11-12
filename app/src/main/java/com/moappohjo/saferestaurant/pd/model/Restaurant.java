0e8279c67fa95d68f803d53b465909c8dbdf5614
ljh7857 <ljh7857@gmail.com>
2020-11-13 01:32:37 +0900

    add getDistance func (Straight distance)

--- app/src/main/java/com/moappohjo/saferestaurant/pd/model/Restaurant.java
+++ app/src/main/java/com/moappohjo/saferestaurant/pd/model/Restaurant.java
@@ -12,6 +12,8 @@ public class Restaurant implements Comparable<Restaurant> {
     public final String type;
     public final String tell;
     public int image;
+    public double distance;
+
 
     public Restaurant(int id, String name, String address, String type, String tell) {
         this.name = name;
@@ -20,6 +22,7 @@ public class Restaurant implements Comparable<Restaurant> {
         this.tell = tell;
         this.image = imageOf(type);
         this.id = id;
+
     }
 
     private int imageOf(String type) {
@@ -42,4 +45,35 @@ public class Restaurant implements Comparable<Restaurant> {
     public boolean equals(@Nullable Object obj) {
         return super.equals(obj);
     }
+
+
+    //인자 1,2는 현재 사용자 위경도, 3,4는 식당 위경도, 5는 단위
+    public double getDistance(double lat1, double lon1, double lat2, double lon2, String unit) {
+
+        double theta = lon1 - lon2;
+        double dist = Math.sin(degTorad(lat1)) * Math.sin(degTorad(lat2)) + Math.cos(degTorad(lat1)) * Math.cos(degTorad(lat2)) * Math.cos(degTorad(theta));
+
+        dist = Math.acos(dist);
+        dist = radTodeg(dist);
+        dist = dist * 60 * 1.1515;
+
+        if (unit == "kilometer") {
+            dist = dist * 1.609344;
+        } else if(unit == "meter"){
+            dist = dist * 1609.344;
+        }
+
+        return (dist);
+    }
+
+    // This function converts decimal degrees to radians
+    private static double degTorad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    // This function converts radians to decimal degrees
+    private static double radTodeg(double rad) {
+        return (rad * 180 / Math.PI);
+    }
+
 }
